8c4d1328ffc940dd28131062243f692c582d7b4c
Kosalvireak Khoeun <virakvary@gmail.com>
2022-07-21 09:38:21 +0700

    math_radom

---
+++ built-in_math.js
@@ -0,0 +1,6 @@
+// random number between 1 and 0
+let me = Math.random();
+console.log(me)
+// if we want bigger number, we just simply multiply by any number
+let men = Math.random()*5463728;
+console.log(men)
--- console_log.js
+++ console_log.js
@@ -6,3 +6,5 @@ console.log('Woohoo! I love to code! #codecademy');
 console.log(20.49);
 // Print stirng len by using .length
 console.log("This is the test string".length);
+// Dalete unwanted space .trim
+console.log("  Hello    you".trim());
