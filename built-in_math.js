7d9a4cfe18db77aa0277beac8962d6513786f13f
Kosalvireak Khoeun <virakvary@gmail.com>
2022-07-21 09:40:30 +0700

    math.ceil

--- built-in_math.js
+++ built-in_math.js
@@ -4,3 +4,6 @@ console.log(me)
 // if we want bigger number, we just simply multiply by any number
 let men = Math.random()*5463728;
 console.log(men)
+//math ceil to remove decimal point and return the bigger number
+let a = Math.ceil(43.9);
+console.log(a);
