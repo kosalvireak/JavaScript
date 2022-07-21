911539f6036c6ac5522b005a0a136618f0da66d8
Kosalvireak Khoeun <virakvary@gmail.com>
2022-07-21 09:34:12 +0700

    console stuff

---
+++ console_log.js
@@ -0,0 +1,8 @@
+console.log("Hello mr." + 007);
+// We don't have to specific data type when log out.
+console.log('JavaScript');
+console.log(2011);
+console.log('Woohoo! I love to code! #codecademy');
+console.log(20.49);
+// Print stirng len by using .length
+console.log("This is the test string".length);
