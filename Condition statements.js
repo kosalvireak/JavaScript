8a01bdcf9ae499de6ffc925da1186cad7a939f6f
Kosalvireak Khoeun <virakvary@gmail.com>
2022-07-21 10:32:03 +0700

    finished

--- Condition statements.js
+++ Condition statements.js
@@ -1,19 +1,58 @@
-// If statement
+// ⁡⁣⁢​‌‌‌If statement
+
 let you = 19;
 let me = 19;
 if (you = me) {
-    console.log("WE are couple");
+  console.log("WE are couple");
 }
 
 
-// If...Else Statements
+// ​‌‌‌If...Else Statements
+
 let sale = false;
 sale = true;
 if (sale) {
-    console.log("Time to buy");
+  console.log("Time to buy");
 }
 else {
-    console.log("Not time to buy");
+  console.log("Not time to buy");
 }
-//------Another way------//
+//​‌‌‌------Another way------//
+
 sale ? console.log("Time to eat") : console.log("Not time to eat");
+
+
+//​‌‌‌Else If Statements
+
+let season = 'summer';
+if (season === 'spring') {
+  console.log('It\'s spring! The trees are budding!');
+} else if (season === 'winter') {
+  console.log('It\'s winter! Everything is covered in snow.');
+} else if (season === 'fall') {
+  console.log('It\'s fall! Leaves are falling!');
+} else if (season === 'summer') {
+  console.log('It\'s sunny and warm because it\'s summer!')
+}
+else {
+  console.log('Invalid season.');
+}
+
+
+//​‌‌‌Switch case
+
+let athleteFinalPosition = 'first place';
+switch (athleteFinalPosition) {
+  case 'first place':
+    console.log('You get the gold medal!');
+    break;
+  case 'second place':
+    console.log('You get the silver medal!');
+    break;
+  case 'third place':
+    console.log('You get the bronze medal!');
+    break;
+  default:
+    console.log('No medal awarded.');
+    break;
+}
---
+++ Ternary Operator.js
@@ -0,0 +1,8 @@
+//Another way of express if.. else statement. 
+if (5>3){
+	console.log("big");
+}else{
+	console.log("small");
+}
+5>3 ? console.log('big'):console.log('small');
+//seperate these 2 expression by :
