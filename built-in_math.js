9215f7f34088f986a05d4bfbf11c558e0b333273
Kosalvireak Khoeun <virakvary@gmail.com>
2022-07-21 09:54:35 +0700

    finish math

--- built-in_math.js
+++ built-in_math.js
@@ -1,9 +1,39 @@
+// Documentation
+// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Math //
 // random number between 1 and 0
 let me = Math.random();
-console.log(me)
+console.log(me)                 //0.05565306146211291
+
 // if we want bigger number, we just simply multiply by any number
-let men = Math.random()*5463728;
-console.log(men)
-//math ceil to remove decimal point and return the bigger number
-let a = Math.ceil(43.9);
-console.log(a);
+let men = Math.random() * 5463728;
+console.log(men)                //4094930.834710306
+
+// rounds the number to a higher value
+console.log(Math.ceil(43.9));   //44
+// rounds the number to a smller value
+console.log(Math.floor(43.9));  //43
+
+// math pow
+console.log(Math.pow(2, 4));    //16
+
+// return absolute value 
+console.log(Math.abs(-5.46543));
+Math.abs('-1');                 // 1
+Math.abs(-2);                   // 2
+Math.abs(null);                 // 0
+Math.abs('');                   // 0
+Math.abs([]);                   // 0
+Math.abs([2]);                  // 2
+Math.abs([1, 2]);               // NaN
+Math.abs({});                   // NaN
+Math.abs('string');             // NaN
+Math.abs();                     // NaN
+
+//return the cube root
+console.log(Math.cbrt(6));      //1.817120
+console.log(Math.cbrt(8));      //2
+
+// return the code
+console.log(Math.cos(50));      //0.9649660284921133
+
+
