86a1ef6e7587231c3f76a0bdcf0aaede38ac69ce
Kosalvireak Khoeun <virakvary@gmail.com>
2022-07-21 10:23:24 +0700

    condition statement

---
+++ Condition statements.js
@@ -0,0 +1,19 @@
+// If statement
+let you = 19;
+let me = 19;
+if (you = me) {
+    console.log("WE are couple");
+}
+
+
+// If...Else Statements
+let sale = false;
+sale = true;
+if (sale) {
+    console.log("Time to buy");
+}
+else {
+    console.log("Not time to buy");
+}
+//------Another way------//
+sale ? console.log("Time to eat") : console.log("Not time to eat");
