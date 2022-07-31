7b100ef280d7a293cdf09e8ff875beec8b7ef229
Kosalvireak Khoeun <virakvary@gmail.com>
2022-07-31 15:57:59 +0700

    Nested loop

--- loop.js
+++ loop.js
@@ -9,6 +9,7 @@ for (let loop = 5; loop<=10; loop++ ){
   9
   10*/
 
+
 //Loop in reverse
 for (let count = 3 ; count >= 0; count -- ){
   console.log(count);
@@ -18,6 +19,7 @@ for (let count = 3 ; count >= 0; count -- ){
   1
   0*/
   
+
 //loop through array
 const me = ['G63','Harley','Scooter']
 for (i in me){
@@ -27,3 +29,16 @@ for (i in me){
   Harley is my favorite.
   Scooter is my favorite.*/
 
+
+//Nested loop -> find mutual friend between 2 array
+const bobsFollowers = ['a','b','d','e'];
+const tinasFollowers = ['a','b','e'];
+let mutual = [];
+for (let i = 0; i <= bobsFollowers.length ;i++){
+  for(let j = 0 ; j <= tinasFollowers.length;j++){
+    if (bobsFollowers[i]===tinasFollowers[j]){
+      mutual.push(bobsFollowers[i]);
+    }
+  }
+}
+console.log(mutual); // s[ 'a', 'b', 'e', undefined ]
