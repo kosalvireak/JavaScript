01617c348b2d9da8dfaf7d4283a8bf7f086e243c
Kosalvireak Khoeun <virakvary@gmail.com>
2022-07-31 15:46:39 +0700

    loop

---
+++ loop.js
@@ -0,0 +1,19 @@
+for (let loop = 5; loop<=10; loop++ ){
+    console.log(loop);
+  }
+  /*Output:
+  5
+  6
+  7
+  8
+  9
+  10*/
+
+//Loop in reverse
+for (let count = 3 ; count >= 0; count -- ){
+  console.log(count);
+}
+  /*3
+  2
+  1
+  0*/
